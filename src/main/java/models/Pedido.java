208f77110a9760dec5da2b7a82451a90608a9394
VITTOR <vittor.giordani>
2025-04-03 21:59:58 -0300

    criando arquivos de model cliente,pedido e Saborpizza

---
+++ src/main/java/models/Pedido.java
@@ -0,0 +1,73 @@
+package models;
+import java.util.List;
+
+public class Pedido {
+
+
+    public class Pedido {
+        private int id;
+        private Cliente cliente;
+        private List<SaborPizza> sabores;
+        private Borda borda;
+        private double valorTotal;
+        private Status status;
+
+        public enum Status {
+            RECEBIDO,
+            EM_PREPARO,
+            PRONTO_PARA_RETIRADA,
+            SAIU_PARA_ENTREGA,
+            ENTREGUE
+        }
+
+        // Getters e Setters
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Cliente getCliente() {
+            return cliente;
+        }
+
+        public void setCliente(Cliente cliente) {
+            this.cliente = cliente;
+        }
+
+        public List<SaborPizza> getSabores() {
+            return sabores;
+        }
+
+        public void setSabores(List<SaborPizza> sabores) {
+            this.sabores = sabores;
+        }
+
+        public Borda getBorda() {
+            return borda;
+        }
+
+        public void setBorda(Borda borda) {
+            this.borda = borda;
+        }
+
+        public double getValorTotal() {
+            return valorTotal;
+        }
+
+        public void setValorTotal(double valorTotal) {
+            this.valorTotal = valorTotal;
+        }
+
+        public Status getStatus() {
+            return status;
+        }
+
+        public void setStatus(Status status) {
+            this.status = status;
+        }
+    }
+
+}
